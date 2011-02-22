836c7cf0b22874903a44c1733a23d2fc735a4de5
Gareth Marshall <garethm@acm.org>
2011-02-22 22:13:25 +1000

    Use two slots to store values

--- lib/cuckoo.rb
+++ lib/cuckoo.rb
@@ -4,7 +4,7 @@ module Cuckoo
       @size = 13
       @store = Array.new @size
       @hash_params1 = {:seed => 5381, :shift => 5}
-      @hash_params2 = {:seed => 5381, :shift => 5}
+      @hash_params2 = {:seed => 4331, :shift => 5}
     end
 
     def djb_hash hash_params, str
@@ -19,12 +19,20 @@ module Cuckoo
       hash = djb_hash @hash_params1, key
       entry = @store[hash]
       raise "Already defined" if entry != nil && entry[0] == key
-      raise "Hash slot taken" if entry != nil && entry[0] != key
+      if entry != nil && entry[0] != key then
+        hash = djb_hash @hash_params2, key
+        entry = @store[hash]
+        raise "Already defined" if entry != nil && entry[0] == key
+        raise "No suitable slot was found" if entry != nil && entry[0] != key
+      end
       @store[hash] = [key, value]
     end
 
     def find key
       hash = djb_hash @hash_params1, key
+      entry = @store[hash]
+      return entry[1] if entry != nil && entry[0] == key
+      hash = djb_hash @hash_params2, key
       entry = @store[hash]
       return entry[1] if entry != nil && entry[0] == key
       nil
