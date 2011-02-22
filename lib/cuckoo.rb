953c0e26600034b73efd5f27738ad0147b1d323f
Gareth Marshall <garethm@acm.org>
2011-02-22 22:11:49 +1000

    Moving towards a parameterized hash

--- lib/cuckoo.rb
+++ lib/cuckoo.rb
@@ -3,18 +3,20 @@ module Cuckoo
     def initialize
       @size = 13
       @store = Array.new @size
+      @hash_params1 = {:seed => 5381, :shift => 5}
+      @hash_params2 = {:seed => 5381, :shift => 5}
     end
 
-    def djb_hash str
-      hash = 5381
+    def djb_hash hash_params, str
+      hash = hash_params[:seed]
       str.bytes.each do |b|
-        hash = ((hash << 5) + hash) + b
+        hash = (((hash << hash_params[:shift]) + hash) + b) % 4294967296
       end
       hash % @size
     end
 
     def add key, value
-      hash = djb_hash key
+      hash = djb_hash @hash_params1, key
       entry = @store[hash]
       raise "Already defined" if entry != nil && entry[0] == key
       raise "Hash slot taken" if entry != nil && entry[0] != key
@@ -22,9 +24,10 @@ module Cuckoo
     end
 
     def find key
-      entry = @store[djb_hash key]
-      return nil if entry == nil
-      entry[1]
+      hash = djb_hash @hash_params1, key
+      entry = @store[hash]
+      return entry[1] if entry != nil && entry[0] == key
+      nil
     end
   end
 end
