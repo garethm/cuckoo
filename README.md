908958c2d527f243ccf0a27769516f55d8473081
Gareth Marshall <garethm@acm.org>
2011-02-22 22:06:08 +1000

    Converted to implement a basic hash table that fails on hash collision

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Cuckoo
 
-This is a little project to try implementing a cuckoo hash data structure.
+This is a little project to try implementing a cuckoo hash data structure. The aim is to explore the algorithm
+rather than to produce something of actual use. I think though that I will implement a C++ version after I'm
+done with this one, which might be more interesting.
+
+Honestly though, if you need to a hash table in Ruby, use the built in ones - they will be way faster and better.
 
 http://programmingpraxis.com/2011/02/01/cuckoo-hashing/
--- lib/cuckoo.rb
+++ lib/cuckoo.rb
@@ -1,16 +1,30 @@
 module Cuckoo
   class Cuckoo
     def initialize
-      @hash = {}
+      @size = 13
+      @store = Array.new @size
+    end
+
+    def djb_hash str
+      hash = 5381
+      str.bytes.each do |b|
+        hash = ((hash << 5) + hash) + b
+      end
+      hash % @size
     end
 
     def add key, value
-      raise "Already defined" if @hash.include? key
-      @hash[key] = value
+      hash = djb_hash key
+      entry = @store[hash]
+      raise "Already defined" if entry != nil && entry[0] == key
+      raise "Hash slot taken" if entry != nil && entry[0] != key
+      @store[hash] = [key, value]
     end
 
     def find key
-      @hash[key]
+      entry = @store[djb_hash key]
+      return nil if entry == nil
+      entry[1]
     end
   end
 end
