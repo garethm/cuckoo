a163448397a8145e80602e84a5a2cbce311eb0e8
Gareth Marshall <garethm@acm.org>
2011-02-22 22:34:59 +1000

    Implemented better alternative hash function

--- lib/cuckoo.rb
+++ lib/cuckoo.rb
@@ -3,14 +3,23 @@ module Cuckoo
     def initialize
       @size = 13
       @store = Array.new @size
-      @hash_params1 = {:seed => 5381, :shift => 5}
-      @hash_params2 = {:seed => 4321, :shift => 5}
+      @hash1 = method(:djb_hash)
+      @hash2 = method(:bkdr_hash)
     end
 
-    def djb_hash hash_params, str
-      hash = hash_params[:seed]
+    def djb_hash str
+      hash = 5381
       str.bytes.each do |b|
-        hash = (((hash << hash_params[:shift]) + hash) + b) % 4294967296
+        hash = (((hash << 5) + hash) + b) % 4294967296
+      end
+      hash % @size
+    end
+
+    def bkdr_hash str
+      seed = 31
+      hash = 0
+      str.bytes.each do |b|
+        hash = (hash * seed + b) % 4294967296
       end
       hash % @size
     end
@@ -18,9 +27,9 @@ module Cuckoo
     def add_cuckoo key, value, key_history
       print "  Attempting to add #{key}\n"
 
-      hash1 = djb_hash @hash_params1, key
+      hash1 = @hash1.call key
       entry1 = @store[hash1]
-      hash2 = djb_hash @hash_params2, key
+      hash2 = @hash2.call key
       entry2 = @store[hash2]
 
       raise "Already defined" if entry1 != nil && entry1[0] == key
@@ -62,10 +71,10 @@ module Cuckoo
     end
 
     def find key
-      hash = djb_hash @hash_params1, key
+      hash = @hash1.call key
       entry = @store[hash]
       return entry[1] if entry != nil && entry[0] == key
-      hash = djb_hash @hash_params2, key
+      hash = @hash2.call key
       entry = @store[hash]
       return entry[1] if entry != nil && entry[0] == key
       nil
